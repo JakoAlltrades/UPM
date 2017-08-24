552831aba027c3d836714071437ee438263b8147
BrandonCoats <bgcoats1313@gmail.com>
2017-08-24 14:13:27 -0600

    hj

--- upm-swing-master/src/com/_17od/upm/gui/JavaFX/AboutDialog.java
+++ upm-swing-master/src/com/_17od/upm/gui/JavaFX/AboutDialog.java
@@ -20,24 +20,21 @@
  */
 package com._17od.upm.gui.JavaFX;
 
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JSeparator;
-import javax.swing.JTextArea;
 
 import com._17od.upm.util.Translator;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 public class AboutDialog extends EscapeDialog {
 
@@ -50,11 +47,11 @@ public class AboutDialog extends EscapeDialog {
         if (version == null) {
             version = "<version unknown>";
         }
+       
+        
+        //panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
-        Pane panel = new Pane(frame);
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-
-        JTextArea jTextArea = new JTextArea();
+        TextArea textArea = new TextArea();
         StringBuffer aboutText = new StringBuffer();
         aboutText.append("Universal Password Manager\n");
         aboutText.append(version);
@@ -62,25 +59,36 @@ public class AboutDialog extends EscapeDialog {
         aboutText.append("Copyright \u00a9 2005-2013 Adrian Smith & Contributors\n\n");
         aboutText.append("adrian@17od.com\n");
         aboutText.append("http://upm.sourceforge.net");
-        jTextArea.setText(aboutText.toString());
-        jTextArea.setAlignmentX(Component.CENTER_ALIGNMENT);
-        jTextArea.setEditable(false);
-        jTextArea.setFont(new Font("Tahoma", Font.PLAIN, 12));
-        jTextArea.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
-        panel.add(jTextArea);
-        panel.add(new JSeparator());
+        textArea.setText(aboutText.toString());
+        //jTextArea.setAlignmentX(Component.CENTER_ALIGNMENT);
+        //align center
+        textArea.setEditable(false);
+        textArea.setFont(new Font("Tahoma", 12));
+        //textArea.setBorder(new Border());
+        Stage stage = new Stage();
+        Scene scene = new Scene(textArea);
+        stage.setScene(scene);
+//        panel.add(textArea);
+        //panel.add(new JSeparator());
+        Pane pane = new Pane();
+        pane.getChildren().add(scene);
+        //panel.add(Box.createRigidArea(new Dimension(0, 10)));
 
-        panel.add(Box.createRigidArea(new Dimension(0, 10)));
-
-        JButton okButton = new JButton(Translator.translate("ok"));
-        okButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                setVisible(false);
-                dispose();
-            }
+        Button okButton = new Button(Translator.translate("ok"));
+        okButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler(){
+			public void handle(Event arg0) {
+			
+			}
+        	
         });
-        okButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel.add(okButton);
+//        okButton.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                setVisible(false);
+//                dispose();
+//            }
+//        });
+        //okButtn.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pane.getChildren().add(okButton);
 
         panel.add(Box.createRigidArea(new Dimension(0, 10)));
         getRootPane().setDefaultButton(okButton);
